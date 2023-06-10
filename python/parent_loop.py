0ee9170846c0af6878b1ecb0639a97ccbac69ef1
StefanRandow <Stefan0Randow@gmail.com>
2023-06-10 15:04:25 -0500

    Update parent_loop.py

--- python/parent_loop.py
+++ python/parent_loop.py
@@ -7,8 +7,13 @@ import os
 while True:
     now_utc = datetime.now(timezone.utc)
     if now_utc.minute == 00 or now_utc.minute == 30:
-        os.system(r"python tweeter.py")
-        time.sleep(1700)
+        
+        if now_utc.hour == 12 and now_utc.minute == 30:
+            os.system(r"forecast_tweet.py")
+            time.sleep(1700)
+            
+        else:
+            os.system(r"python tweeter.py")
+            time.sleep(1700)
     else:
         time.sleep(59)
-        
